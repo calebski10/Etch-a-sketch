76a0cdc7eb6ce952d042dfea3659b82f42664326
calebski10 <87187565+calebski10@users.noreply.github.com>
2022-05-22 04:20:49 +0100

    init

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const container = document.querySelector('.container')
 const btnBlack = document.createElement('button')
+const btnRgb = document.createElement('button')
 const btnGray = document.createElement('button')
-const btnRGB = document.createElement('button')
 const btnSize = document.createElement('button')
-const buttonscontainer = document.querySelector('.container')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
 
-function createDivs(col, rows) {
-    for(let i=0; i < (col * rows); i++){
-const div = document.createElement('div')
-div.style.border = '1px solid red'
-container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
-container.appendChild(div).classList.add('box')
-}
-}
-createDivs(16,16)
-
-function grayColor(){
+window.onload = () => {
     const boxs = container.querySelectorAll('.box')
-    btnGray.textContent ='Gray'
-    btnGray.addEventListener('click' ,() =>{
-        boxs.forEach(box => box.addEventListener ('mouseover', () =>{
-            let Rnum = Math.floor(Math.random()* 255)
-       box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
+    boxs.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.background = 'black'
+    }))
+}
+
+
+function creatDivs(col , rows) {
+    for(let i = 0;i < (col * rows); i++) {
+        const div = document.createElement('div') 
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box')
+    }
+}
+creatDivs(16,16)
+
+function grayColor() { 
+    
+    const boxs = container.querySelectorAll('.box')
+    btnGray.textContent = "GRAY"
+    btnGray.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
         }))
     })
     buttonsContainer.appendChild(btnGray).classList.add('btn')
@@ -30,57 +40,87 @@ function grayColor(){
 grayColor()
 
 
-function blackColor(){
+function rgbColors() {
+    
     const boxs = container.querySelectorAll('.box')
-    btnBlack.textContent ='Black'
-    btnBlack.addEventListener('click' ,() =>{
-        boxs.forEach(box => box.addEventListener ('mouseover', () =>{
-       box.style.background = 'black'
-        }))
-    })
-    buttonsContainer.appendChild(btnBlack).classList.add('btn')
-}
-blackColor()
-
-
-function rgbColor(){
-    const boxs = container.querySelectorAll('.box')
-    btnRgb.textContent ='Rgb'
-    btnRgb.addEventListener('click' ,() =>{
-        boxs.forEach(box => box.addEventListener ('mouseover', () =>{
-      let R= Math.floor(Math.random()* 255)
-      let G= Math.floor(Math.random()* 255)
-      let B= Math.floor(Math.random()* 255)
-      box.style.background =  `rgb(${R},${G},${B})`
+    btnRgb.textContent = "RGB"
+    btnRgb.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
         }))
     })
     buttonsContainer.appendChild(btnRgb).classList.add('btn')
 }
-rgbColor()
+rgbColors()
 
-function reset() {
+function blackColor() {
+   
     const boxs = container.querySelectorAll('.box')
-    boxs.forEach(box => box.remove())
+    btnBlack.textContent = 'BLACK';
+    btnBlack.addEventListener('click', function () {
+        boxs.forEach(box => box.addEventListener('mouseover', function() {
+            this.style.background = 'black'
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
 }
- function resize(){
-     btnSize.textContent = 'GRID SIZE'
-     btnSize.addEventListener('click',()=>{
-         let user = prompt('what size do you want your grid to be?')
-         if(user ===null||user<1){
-             reset()
-             createDivs(16,16)
-             blackColor()
-             grayColor()
-             rgbColor()
-         }
-         else{
-             reset()
-             createDivs(user,user)
-             blackColor()
-             grayColor()
-             rgbColor()
-         }
-     })
-     buttonsContainer.appendChild(btnSize).classList.add('btn')
- }
- resize()
+blackColor() 
+
+function reSet() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => {
+        box.remove();
+    })
+}
+
+
+function reSize() {
+   
+    btnSize.textContent = 'GRID SIZE'
+    btnSize.addEventListener('click', () => {
+        let user = prompt('WHAT SIZE YOU WANT YOUR GRID TO BE?')
+        if(user === null || user < 1){
+            reSet();
+            creatDivs(16,16);
+            grayColor();
+            rgbColors();
+            blackColor();
+        } else { 
+            reSet();
+            creatDivs(user,user);
+            grayColor();
+            rgbColors();
+            blackColor();
+        }
+    })
+    buttonsContainer.appendChild(btnSize).classList.add('btn')
+}
+reSize()
+
+function backgrooundAnim() {
+
+    colors = ['red','green','blue', 'yellow'];
+    const section = document.querySelector('.section');
+    const span = document.createElement('span');
+
+    let size = Math.random() * 50;
+
+    span.style.width = 10 + size + 'px';
+    span.style.height = 10 + size + 'px';
+    span.style.borderRadius = `${size}%`;
+    span.style.top = Math.random() * innerHeight + "px";
+    span.style.left = Math.random() * innerWidth + "px";
+
+    const bg = colors[Math.floor(Math.random() * colors.length)];
+    span.style.background = bg;
+
+    section.appendChild(span);
+
+    setTimeout(() => {span.remove()},5000)
+}
+
+setInterval(backgrooundAnim, 150);

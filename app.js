e671b7654e115d78902cfb3254d096a3e97e9975
calebski10 <87187565+calebski10@users.noreply.github.com>
2022-05-22 03:56:30 +0100

    init

--- app.js
+++ app.js
@@ -15,15 +15,72 @@ container.appendChild(div).classList.add('box')
 }
 }
 createDivs(16,16)
-function graycolor(){
+
+function grayColor(){
     const boxs = container.querySelectorAll('.box')
     btnGray.textContent ='Gray'
-    btnGray.addEventListener('click',() =>{
+    btnGray.addEventListener('click' ,() =>{
         boxs.forEach(box => box.addEventListener ('mouseover', () =>{
             let Rnum = Math.floor(Math.random()* 255)
-       box.style.background = ` rgb(${Rnum},${Rnum},${Rnum})`
+       box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
         }))
     })
-    buttonscontainer.appendChild(btnGray).classList.add(`btn`)
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
-graycolor()
+grayColor()
+
+
+function blackColor(){
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent ='Black'
+    btnBlack.addEventListener('click' ,() =>{
+        boxs.forEach(box => box.addEventListener ('mouseover', () =>{
+       box.style.background = 'black'
+        }))
+    })
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+blackColor()
+
+
+function rgbColor(){
+    const boxs = container.querySelectorAll('.box')
+    btnRgb.textContent ='Rgb'
+    btnRgb.addEventListener('click' ,() =>{
+        boxs.forEach(box => box.addEventListener ('mouseover', () =>{
+      let R= Math.floor(Math.random()* 255)
+      let G= Math.floor(Math.random()* 255)
+      let B= Math.floor(Math.random()* 255)
+      box.style.background =  `rgb(${R},${G},${B})`
+        }))
+    })
+    buttonsContainer.appendChild(btnRgb).classList.add('btn')
+}
+rgbColor()
+
+function reset() {
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.remove())
+}
+ function resize(){
+     btnSize.textContent = 'GRID SIZE'
+     btnSize.addEventListener('click',()=>{
+         let user = prompt('what size do you want your grid to be?')
+         if(user ===null||user<1){
+             reset()
+             createDivs(16,16)
+             blackColor()
+             grayColor()
+             rgbColor()
+         }
+         else{
+             reset()
+             createDivs(user,user)
+             blackColor()
+             grayColor()
+             rgbColor()
+         }
+     })
+     buttonsContainer.appendChild(btnSize).classList.add('btn')
+ }
+ resize()
